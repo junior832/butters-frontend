d6f8d5cf792bab81dd8f96883e1d87a98e357c87
Gerardo Mares <gerardomares94@gmail.com>
2024-06-20 20:18:29 -0500

    refactor: linted js files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Test from './components/test';
+import Test from './components/test.js';
 
 const App = () => {
   return (
-    <div>App
-        <Test />
+    <div>
+      App
+      <Test />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App.js';
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
